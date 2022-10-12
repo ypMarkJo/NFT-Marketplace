a94d439e2e93b9455d5858a667c797c211168419
Jo Youngpyeong <80379900+ypMarkJo@users.noreply.github.com>
2022-10-12 17:59:55 +0900

    Update app.js
    eliminate secure factor

--- NFT-Marketplace-BE/app.js
+++ NFT-Marketplace-BE/app.js
@@ -21,9 +21,7 @@ db.on('error', console.error);
 db.once('open', () => {
   console.log('Connected to mongodb server');
 });
-// mongoose.connect('mongodb://10.0.8.80:27017/blockchaindb')
-//   .then(() => console.log('MongoDB Connected...'))
-//   .catch((err) => console.log(err));
+
 
 mongoose.connect('mongodb://localhost:27017/blockchaindb')
   .then(() => console.log('MongoDB Connected...'))
